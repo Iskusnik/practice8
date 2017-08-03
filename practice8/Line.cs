f8a751f7d55867757c87dcc2814943394bca52d5
IskusnikXD <IskusnikXD@ISKUSNIK>
2017-08-03 13:56:49 +0500

    1. В процессе генератор тестов 2. Разработан класс ребра, содержащий информацию о инциндентных вершинах.

---
+++ practice8/Line.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice8
+{
+   
+    class Line
+    {
+        public string A { get; set; }
+        public string B { get; set; }
+        public Line(string a, string b)
+        {
+            if (CompareStrings(A, B))
+            {
+                A = a;
+                B = b;
+            }
+            else
+            {
+                B = a;
+                A = b;
+            }
+        }
+        public override int GetHashCode()
+        {
+            return ((A + B).GetHashCode());
+        }
+        public override bool Equals(object obj)
+        {
+            if (((Line)obj).GetHashCode() == this.GetHashCode())
+                return true;
+            else
+                return false;
+        }
+
+        static public bool CompareStrings(string s1, string s2)//s1 < s2 = true
+        {
+            for (int i = 0; i < s1.Length; i++)
+                if (i == s2.Length)
+                    return false;
+                else
+                if (s1[i] < s2[i])
+                    return true;
+
+            return false;
+        }
+
+    }
+}
---
+++ practice8/TestGen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice8
+{
+    //Генерация N рёбер
+    //Рёбра разные
+    //Ребро AA - возможно
+
+    class TestGen
+    {
+        static Line[] graph = new Line[10001];
+
+        static Random random = new Random();
+        static public Line newLine(int N)
+        {
+            Line line;
+            do
+                line = new Line(char.ConvertFromUtf32(random.Next(0, N) + 'A'), char.ConvertFromUtf32(random.Next(0, N) + 'A'));
+            while (graph[line.GetHashCode() % 10001] != null); //Надо убрать генерацию двух одинаковых рёбер
+
+            graph[line.GetHashCode() % 10001] = line;
+            return line;
+        }
+    }
+}
